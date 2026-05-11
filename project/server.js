5625185915fffdc63464e461d27cc111fcbf1541
Vishwajith JS <jsvishwajith@gmail.com>
2026-05-11 12:17:27 +0530

    updated API URLs

--- project/server.js
+++ project/server.js
@@ -1,9 +1,7 @@
-require("dotenv").config()
-
 const express = require("express")
+const mongoose = require("mongoose")
 const cors = require("cors")
-
-const connectDB = require("./config/db")
+require("dotenv").config()
 
 const authRoutes = require("./routes/authRoutes")
 const articleRoutes = require("./routes/articleRoutes")
@@ -13,14 +11,16 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-connectDB()
-
-app.get("/", (req, res) => {
-    res.send("Knowledge Base API Running")
-})
-
 app.use("/api/auth", authRoutes)
 app.use("/api/articles", articleRoutes)
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log("MongoDB Connected"))
+    .catch((err) => console.log(err))
+
+app.get("/", (req, res) => {
+    res.send("Backend Running")
+})
 
 const PORT = process.env.PORT || 5000
 
